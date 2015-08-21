d2311399df0b68af5da64baa35a49f6991e0fdb8
Erik Castricum <erikcas1972@gmail.com>
2015-08-21 17:55:16 +0200

    rhine: remove memconfig for 3.4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=rhine androidboot.selinux=0
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=192598
 BOARD_KERNEL_CMDLINE += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
 BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
-BOARD_KERNEL_CMDLINE += coherent_pool=8M vmalloc=400M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=0
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
