fd3c809f1661136f552c4160103ec7cfd49cc908
erikcas <erikcas1972@gmail.com>
2015-11-15 21:31:59 +0100

    Revert "camera from source"
    This reverts commit 93da738846858872bd3afcdf30c62a6d36b4c116.

--- device.mk
+++ device.mk
@@ -156,8 +156,6 @@ PRODUCT_PACKAGES += \
     libmmcamera_interface \
     libmmjpeg_interface \
     libqomx_core \
-    libmm-qcamera \
-    mm-qcamera-app \
     camera.msm8974
 
 PRODUCT_PACKAGES += \
